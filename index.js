57b85330b7229b02ad19bd4a5db63d38e62f560d
Marius Sprenger <mariussprenger@mariuss-mbp.localdomain>
2021-02-10 15:32:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = () => {
+    return console.log(
+`Glad you found us. 
+We are always looking for great devevlopers at lemon.markets that are interested in looking behind the curtain and finding out how things work.
+Do you see yourself in any of the roles below?
+
+Frontend Developer:
+
+Together with Marius you will create all customer facing applications of lemon.markets. We believe that a great product also requires a great design and a great feeling when it's used. So, to cater our community an awesome user experience and to create the most developer-friendly API for trading and investing at the stock market, we need to build a sleek website, dashboard and (for most) an astonishing documentation that anyone can easily understand. 
+---
+
+(Senior) Backend Engineer:
+
+You will operate at the heart of lemon.markets - the API. The (RESTful) API is a composition of multiple micro-services. Some of them are publicly available, some of them operate under the hood. This means you will basically write code, so other developers can build and rely on your software. Therefore, we like to use NodeJS, especially for asynchronous tasks. Whereas for synchronous things, we like to use Python (oftentimes in combination with Django). Sometimes this can also involve helping our customers with their projects or contributing to SDKs and other open source tools for the community.This role will give you a lot of opportunities to shape our product going forward and the freedom to create SDKs, content or other tools for our community along the way.
+---
+
+Infrastructure Engineer:
+
+We are hosted in the cloud with AWS, but love to use (managed) open source technology for most things. This enables us to offer the highest reliability to our demanding customers (developers 😉). Therefore, we make no compromises, aiming for 100% uptime. Monitoring all services and automatically alerting the right people, while constantly optimizing processes and tools will be a part of your job, too. To safeguard the security of our customers, you will help operate our private subnets, while keeping track of internal security preventing issues inside the organization as well. Together with our backend team, you will create a suitable pipeline to push code to production using certain stages to ship quality code and not lose speed while releasing great features.
+---
+
+Unexpected Awesome Person:
+
+The title of this page puts it very well. This is not about a specific role, but about you as a character! Even though we are only actively looking for people to join our engineering team right now, this does not mean that we don't need other roles to assemble a rockstar formation to change the brokerage industry. In the next months we will be looking for a couple of profiles joining us, so we are more than happy to get to know you as early as possible and see if we can bring you in. It does not matter if you are experienced in Marketing, Data Engineering, Business Development, Business Intelligence, Brokerage, Legal & Compliance or want to help us to support our developer community with unique content. We would love to hear from you!
+---
+
+Interested?
+Come visit https://www.notion.so/Welcome-to-lemon-markets-b371efff6ffe4803b9cb1aba5df39bf3 to find out if we are a great match.`);
+}
