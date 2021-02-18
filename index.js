e0aa8c11271f8aa2cc92779f3cc56ae32377a6a7
Marius Sprenger <marius.p.sprenger@gmail.com>
2021-02-18 16:26:47 +0100

    updates description

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ Do you see yourself in any of the roles below?
 
 Frontend Developer (React):
 
-Develop the web app | homepage | documentation from scratch + all future customer facing applications.  
+Develop the web app | homepage | documentation from scratch + all future customer facing applications. 
+More info: https://www.notion.so/Frontend-Engineer-00ef0295e22d4210b686bb07047be0a2 
 ---
 
 (Senior) Backend Engineer (Python, Django, node.js):
 
 Build the real-money trading API and our Market Data Services.
+More info: https://www.notion.so/Senior-Backend-Engineer-c49c74a5d55c41dd918021d6fc61b3d2
 
 ---
 
 DevOps and Infrastructure Engineer (AWS, Terraform):
 
 Make sure that our services are always up and running - all day everyday.
+More info: https://www.notion.so/Infrastructure-Engineer-7f2e5cda0bfd40ee813083c0386735a2
 ---
 
 Unexpected Awesome Person:
 
 Come and surprise us with your special talent - whether it's business strategy, design, marketing, or any other relevant field.
+More info: https://www.notion.so/Unexpected-Awesome-Person-fb21e2f7e9c74095b157427692515021
 ---
 
 Interested? --> info@lemon.markets
